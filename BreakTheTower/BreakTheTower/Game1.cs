b3bdbce2e5bc77040f3eebc68367921d742d7eaf
jcomo <jamescomo44@gmail.com>
2022-08-23 13:06:14 -0700

    Create and use Textured 3D Quad

--- BreakTheTower/BreakTheTower/Game1.cs
+++ BreakTheTower/BreakTheTower/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using BreakTheTower.Primitives3D;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -9,6 +10,10 @@ namespace BreakTheTower
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Camera3D __camera;
+        private Texture2D __islandTexture;
+        private Quad __texturedQuad;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -18,7 +23,12 @@ namespace BreakTheTower
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            __camera = new Camera3D(GraphicsDevice.Viewport.AspectRatio)
+            {
+                Position = new Vector3(0, -2f, 2f)
+            };
+
+            __texturedQuad = new Quad(Vector3.Zero, Vector3.Backward, Vector3.Up, 1, 1);
 
             base.Initialize();
         }
@@ -27,24 +37,30 @@ namespace BreakTheTower
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            __islandTexture = Content.Load<Texture2D>("Test_Island");
+            __texturedQuad.InitializeEffect(GraphicsDevice, __camera, __islandTexture);
         }
 
+        KeyboardState prevKeyboardState;
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.O) && !prevKeyboardState.IsKeyDown(Keys.O))
+                __camera.Orbit = !__camera.Orbit;
 
+            __camera.Update();
+
+            prevKeyboardState = Keyboard.GetState();
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.LightBlue);
 
-            // TODO: Add your drawing code here
+            __texturedQuad.Draw(GraphicsDevice, __camera);
 
             base.Draw(gameTime);
         }
---
+++ BreakTheTower/BreakTheTower/Primitives3D/Quad.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreakTheTower.Primitives3D
+{
+    public class Quad
+    {
+        public readonly VertexPositionNormalTexture[] Vertices;
+        public readonly short[] Indexes;
+
+        public BasicEffect Effect { get; set; }
+
+        private Vector3 _origin;
+        private Vector3 _upperLeft;
+        private Vector3 _lowerLeft;
+        private Vector3 _upperRight;
+        private Vector3 _lowerRight;
+        private Vector3 _normal;
+        private Vector3 _up;
+        private Vector3 _left;
+
+        public Quad(Vector3 origin, Vector3 normal, Vector3 up, float width, float height)
+        {
+            Vertices = new VertexPositionNormalTexture[4];
+            Indexes = new short[6];
+
+            _origin = origin;
+            _normal = normal;
+            _up = up;
+
+            CalculateCorners(width, height);
+            FillVertices();
+        }
+
+        private void CalculateCorners(float width, float height)
+        {
+            _left = Vector3.Cross(_normal, _up);
+
+            Vector3 upperCenter = (_up * height / 2) + _origin;
+            _upperLeft = upperCenter + (_left * width / 2);
+            _upperRight = upperCenter - (_left * width / 2);
+            _lowerLeft = _upperLeft - (_up * height);
+            _lowerRight = _upperRight - (_up * height);
+        }
+
+        private void FillVertices()
+        {
+            Vector2 textureUpperLeft = new Vector2(0.0f, 0.0f);
+            Vector2 textureLowerLeft = new Vector2(0.0f, 1.0f);
+            Vector2 textureUpperRight = new Vector2(1.0f, 0.0f);
+            Vector2 textureLowerRight = new Vector2(1.0f, 1.0f);
+
+            for (int i = 0; i < Vertices.Length; i++)
+                Vertices[i].Normal = _normal;
+
+            Vertices[0].Position = _lowerLeft;
+            Vertices[1].Position = _upperLeft;
+            Vertices[2].Position = _lowerRight;
+            Vertices[3].Position = _upperRight;
+
+            Vertices[0].TextureCoordinate = textureLowerLeft;
+            Vertices[1].TextureCoordinate = textureUpperLeft;
+            Vertices[2].TextureCoordinate = textureLowerRight;
+            Vertices[3].TextureCoordinate = textureUpperRight;
+
+            // Indexed using Clockwise Winding.
+            Indexes[0] = 0;
+            Indexes[1] = 1;
+            Indexes[2] = 2;
+            Indexes[3] = 2;
+            Indexes[4] = 1;
+            Indexes[5] = 3;
+        }
+
+        public void InitializeEffect(GraphicsDevice graphics, Camera3D camera, Texture2D texture)
+        {
+            Effect = new BasicEffect(graphics)
+            {
+                View = camera.View,
+                World = camera.World,
+                Projection = camera.Projection
+            };
+
+            Effect.Texture = texture;
+            Effect.TextureEnabled = true;
+        }
+
+        public void Draw(GraphicsDevice graphics, Camera3D camera)
+        {
+            if (Effect is null)
+                throw new InvalidOperationException("Attempted to draw a Quad without calling InitializeEffect first.");
+
+            Effect.View = camera.View;
+
+            BlendState oldBlendState = graphics.BlendState;
+            graphics.BlendState = BlendState.AlphaBlend;
+
+            foreach (EffectPass pass in Effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                graphics.DrawUserIndexedPrimitives<VertexPositionNormalTexture>(
+                    PrimitiveType.TriangleList,
+                    Vertices, 0, 4,
+                    Indexes, 0, 2
+                );
+            }
+
+            graphics.BlendState = oldBlendState;
+        }
+    }
+}
